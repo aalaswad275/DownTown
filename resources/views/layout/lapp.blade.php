22fbd0f5a1f83da39321f265e3e1794845d5ef0d
aalaswad275 <aalaswad275@gmail.com>
2025-12-21 16:18:58 +0200

    today

--- resources/views/layout/lapp.blade.php
+++ resources/views/layout/lapp.blade.php
@@ -105,7 +105,7 @@
                         </div>
                         <a href="{{route('contact')}}" class="nav-item nav-link">{{__('Contact')}}</a>
                     </div>
-                    
+
                 </div>
             </nav>
 
@@ -224,13 +224,13 @@
             <div class="container">
                 <div class="row g-4 align-items-center">
                     <div class="col-md-6 text-center text-md-end mb-md-0">
-                        <i class="fas fa-copyright me-2"></i><a class="text-white" href="#">Your Site Name</a>, All right reserved.
+                        <i class="fas fa-copyright me-2"></i><a class="text-white" href="{{route('welcome')}}">{{__('Libya Land')}}</a>, {{__('All right reserved.')}}
                     </div>
-                    <div class="col-md-6 text-center text-md-start">
+                    <div class="col-md-6 text-center text-md-start copy">
                         <!--/*** This template is free as long as you keep the below author’s credit link/attribution link/backlink. ***/-->
                         <!--/*** If you'd like to use the template without the below author’s credit link/attribution link/backlink, ***/-->
                         <!--/*** you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". ***/-->
-                        Designed By <a class="text-white" href="https://htmlcodex.com">HTML Codex</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a>
+                        {{__('Designed and  Distributed By')}} <a href="#">{{__('SheCode Team')}}</a>
                     </div>
                     </div>
                 </div>
